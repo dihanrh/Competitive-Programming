56c0158ebfa026a8b2de1f84be1d2027df90eee9
dihanrh <code.dihan@gmail.com>
2022-04-22 23:07:22 +0600

    Codeforces ED Round Please enter the commit message for your changes. Lines starting

---
+++ StringBuilding.cpp
@@ -0,0 +1,51 @@
+// https://codeforces.com/contest/1671/problem/A
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main ()
+{   
+            ios::sync_with_stdio(0);
+            cin.tie(0);
+            cout.tie(0);
+
+             int tc ; 
+             cin >> tc  ; 
+             while ( tc -- )
+             {
+                  string s ; 
+                  cin >> s ; 
+                  bool f = false ;
+                  bool ff = false ;
+                  int ca = 1 , cb = 0  ;
+
+                  for ( int i = 1 ; i < s.size() ; i ++)
+                    {
+                        
+                        if (s[i] == s[i-1])
+                        {
+                            ca ++ ; 
+                        }
+                        else 
+                        {
+                            if ( ca == 1) 
+                            {
+                                break ;
+                                
+                            }
+                            ca = 1 ; 
+                        }
+                        
+                    }
+
+
+                    if (ca == 1)
+                    {
+                        cout <<"NO" << "\n" ;
+                    }
+                    else
+                    {
+                        cout << "YES" << "\n" ;
+                    }
+    }
+    return 0 ;
+}
