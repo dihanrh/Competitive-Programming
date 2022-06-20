725cef01e84824a2b8628eb66ce70a8efa8a9082
Rakibul Hasan Dihan <98779204+dihanrh@users.noreply.github.com>
2022-06-21 02:01:38 +0600

    Codeforces Round #782 (Div. 2)

---
+++ RedVersusBlue.cpp
@@ -0,0 +1,50 @@
+//https://codeforces.com/contest/1659/problem/A
+#include <bits/stdc++.h>
+#define FAST ios::sync_with_stdio(0) ; cin.tie(0); cout.tie(0) ;
+using namespace std ;
+
+
+void solve()
+{
+     int n , r , b ;
+     cin >> n >> r >> b ;
+
+     int rat = r / (b+1) ;
+     int rim = r % (b+1) ;
+
+     for ( int i  = 0 ; i < rim  ; i ++)
+     {
+      cout << string(rat+1 , 'R') << "B" ;
+     }
+     for (int  i = rim ; i < b ; i ++)
+     {
+      cout << string (rat , 'R') << "B" ;
+     }
+     cout << string(rat , 'R') ;
+
+
+
+     cout << "\n" ;
+
+}
+
+void testCase ()
+{
+   long long int t ;
+   cin >> t ;
+
+   while (t --)
+   {
+      solve() ; 
+   }
+}
+
+
+int main ()
+{   
+   FAST ;
+     
+      testCase () ;
+             
+    return 0 ;
+}
