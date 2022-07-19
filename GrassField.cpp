30d3818a3313e255304f9e20f181a0a83e515fad
Rakibul Hasan Dihan <98779204+dihanrh@users.noreply.github.com>
2022-07-19 22:13:24 +0600

    Educational Codeforces Round 131 (Rated for Div. 2)

---
+++ GrassField.cpp
@@ -0,0 +1,55 @@
+//https://codeforces.com/contest/1701/problem/A
+#include <bits/stdc++.h>
+#define FAST ios::sync_with_stdio(0) ; cin.tie(0); cout.tie(0) ;
+using namespace std ;
+
+
+void solve()
+{
+    int r = 0  , c  = 0 , n  ; 
+
+   for ( int i = 0  ; i < 4 ; i ++)
+   {
+      cin >> n ;
+      if (i < 2 )
+      {
+         r += n ;
+      }
+      else 
+      {
+         c += n ;
+      }
+   }
+
+   int ans = 0 ;
+   if ((r == 0 and c != 0) or (r != 0 and c == 0) )
+   {
+   	ans =  1 ;
+   }
+   else 
+   {
+   	ans =  min ( r , c ) ; 
+   }
+   cout << ans << "\n" ;
+}
+
+void testCase ()
+{
+   long long int t ;
+   cin >> t ;
+
+   while (t --)
+   {
+      solve() ; 
+   }
+}
+
+
+int main ()
+{   
+   FAST ;
+     
+      testCase () ;
+             
+    return 0 ;
+}
