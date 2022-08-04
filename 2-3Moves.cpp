ef2c574a0c26c1e635d25978c96dcb7b492ccb09
Rakibul Hasan Dihan <98779204+dihanrh@users.noreply.github.com>
2022-08-05 00:26:42 +0600

    Educational Codeforces Round 133 (Rated for Div. 2)

---
+++ 2-3Moves.cpp
@@ -0,0 +1,48 @@
+//https://codeforces.com/contest/1716/problem/A
+#include <bits/stdc++.h>
+#define FAST ios::sync_with_stdio(0) ; cin.tie(0); cout.tie(0) ;
+using namespace std ;
+
+
+void solve()
+{
+    long long int  n ;
+    cin >> n ; 
+
+    long long int ans = 0 ; 
+
+    ans = n / 3 ;
+    
+    if( n == 1)
+    {
+    	ans ++ ;
+    }
+    
+    if (n%3 !=  0)
+    {
+        ans ++ ;
+    }
+
+    cout << ans << "\n" ;
+}
+
+void testCase ()
+{
+   long long int t ;
+   cin >> t ;
+
+   while (t --)
+   {
+      solve() ; 
+   }
+}
+
+
+int main ()
+{   
+   FAST ;
+     
+      testCase () ;
+             
+    return 0 ;
+}
